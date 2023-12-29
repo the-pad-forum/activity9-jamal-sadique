999a1b38a51200b8f7c0eaa5d1cd7e1377e4516c
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2023-12-29 18:54:59 -0500

    Add icons+fix nav shadow color

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Image from "next/image";
 import Icon from '../Images/favicon-4.png';
 import styles from './Nav.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers, faImage, faComments } from "@fortawesome/free-solid-svg-icons";
+
 
 const Nav = ({ onSectionChange }: { onSectionChange: (section: 'Users' | 'Posts' | 'Albums') => void }) => {
   return (
@@ -9,12 +12,11 @@ const Nav = ({ onSectionChange }: { onSectionChange: (section: 'Users' | 'Posts'
       <Image className={styles.icon} src={Icon} width="45" height="45" alt="" />
       <h1 className={styles.pageTitle}>Next Data Fetching UI</h1>
       <div className={styles.buttonContainer}>
-        <button className={styles.button} onClick={() => onSectionChange('Users')}>Users</button>
-        <button className={styles.button} onClick={() => onSectionChange('Posts')}>Posts</button>
-        <button className={styles.button} onClick={() => onSectionChange('Albums')}>Albums</button>
+        <button className={styles.button} onClick={() => onSectionChange('Users')}> <FontAwesomeIcon style={{color: "black" }} icon={faUsers} /> Users</button>
+        <button className={styles.button} onClick={() => onSectionChange('Posts')}> <FontAwesomeIcon style={{color: "black" }} icon={faComments} /> Posts</button>
+        <button className={styles.button} onClick={() => onSectionChange('Albums')}> <FontAwesomeIcon style={{color: "black" }} icon={faImage} /> Albums</button>
       </div>
-      
-      
+
     </nav>
   );
 }
